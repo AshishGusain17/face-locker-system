fb4563288f821a67a89f1e5750aaad4c94c608ca
AshishGusain17 <57452568+AshishGusain17@users.noreply.github.com>
2020-02-17 20:56:23 +0530

    change

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import logging
 from camera import *
+import cv2
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
